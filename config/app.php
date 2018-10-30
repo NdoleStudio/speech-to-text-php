7fabdc277e39e59459e5e148bb48667e97153368
Acho Arnold <arnold@ndolestudio.com>
2018-10-30 11:35:43 +0100

    Removing the google cloud provider

--- config/app.php
+++ config/app.php
@@ -157,11 +157,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
-        /**
-         * Custom providers.
-         */
-        \Superbalist\LaravelGoogleCloudStorage\GoogleCloudStorageServiceProvider::class,
     ],
 
     /*
